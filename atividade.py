7cdafae080a5283dc46a2388b9a4ce048d63b899
natalyabreu <133766517+natalyabreu@users.noreply.github.com>
2023-06-02 08:10:08 -0300

    Create atividade.py

---
+++ atividade.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd 
+import csv
+st.title('tarefa do josir')
+st.caption('Nataly Abreu')
+
+df = pd.read_csv('acidentes.csv")
+#for linha in arquivo:
+#    st.write(linha)
