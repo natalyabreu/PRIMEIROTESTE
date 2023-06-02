7e972fddba1b00792d3fa58b11f9c60f9141f3a9
natalyabreu <133766517+natalyabreu@users.noreply.github.com>
2023-06-02 08:59:05 -0300

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -5,5 +5,7 @@ st.title('Municípios brasileiros')
 st.caption('Nataly Abreu - Atividade 1')
 
 df = pd.read_csv('brasil-win.csv', encoding='ISO-8859-1')
-st.write(df)
+
 st.dataframe(df)
+
+st.bar_chart(chart_data.set_index)
